9cf79b3ef4c85184f03a38875ff14a08d9c5260e
cat-j <catalinajuarros@gmail.com>
2018-05-03 15:28:53 -0300

    Updated to match superblock-based DogeRelay contracts

---
+++ src/main/java/org/dogethereum/dogesubmitter/core/SuperblockLevelDBBlockStore.java
@@ -0,0 +1,21 @@
+package org.dogethereum.dogesubmitter.core;
+
+import org.dogethereum.dogesubmitter.BridgeUtils;
+import org.dogethereum.dogesubmitter.constants.BridgeConstants;
+
+import org.bitcoinj.store.BlockStore;
+import org.bitcoinj.store.BlockStoreException;
+import org.bitcoinj.core.Context;
+
+import com.google.common.base.Objects;
+
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.util.Locale;
+
+import static com.google.common.base.Preconditions.checkState;
+public class SuperblockLevelDBBlockStore {
+    private static final byte[] CHAIN_HEAD_KEY;
+
+    public SuperblockLevelDBBlockStore(Context context) {}
+}
