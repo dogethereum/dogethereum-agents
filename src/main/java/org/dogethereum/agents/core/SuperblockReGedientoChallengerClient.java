98b40ac96d8e6434900cb13cabae6d540bc55253
cat-j <catalinajuarros@gmail.com>
2018-08-03 16:21:23 -0300

    Make derived class from SuperblockChallengerClient

--- src/main/java/org/dogethereum/agents/core/SuperblockReGedientoChallengerClient.java
+++ src/main/java/org/dogethereum/agents/core/SuperblockReGedientoChallengerClient.java
@@ -1,4 +1,42 @@
 package org.dogethereum.agents.core;
 
-public class SuperblockReGedientoChallengerClient {
+import lombok.extern.slf4j.Slf4j;
+import org.dogethereum.agents.core.eth.EthWrapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j(topic = "SuperblockReGedientoChallengerClient")
+public class SuperblockReGedientoChallengerClient extends SuperblockChallengerClient {
+
+    @Override
+    public long reactToEvents(long fromBlock, long toBlock) {
+        try {
+            validateNewSuperblocks(fromBlock, toBlock);
+            challengeEverything(fromBlock, toBlock);
+            deleteFinishedBattles(fromBlock, toBlock);
+
+            getSemiApproved(fromBlock, toBlock);
+            removeApproved(fromBlock, toBlock);
+            removeInvalid(fromBlock, toBlock);
+
+            synchronized (this) {
+                flushSemiApprovedSet();
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            return latestEthBlockProcessed;
+        }
+        return toBlock;
+    }
+
+    private void challengeEverything(long fromBlock, long toBlock) throws Exception {
+        List<EthWrapper.SuperblockEvent> newSuperblockEvents = ethWrapper.getNewSuperblocks(fromBlock, toBlock);
+        log.info("Challenging everything");
+        for (EthWrapper.SuperblockEvent superblockEvent : newSuperblockEvents) {
+            log.info("Challenging superblock {}", superblockEvent.superblockId);
+            ethWrapper.challengeSuperblock(superblockEvent.superblockId);
+        }
+    }
 }
