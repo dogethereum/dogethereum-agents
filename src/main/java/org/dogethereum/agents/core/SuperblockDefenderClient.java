4f5870665a3175f6fe18c4c00d50c09fe50c1eb3
cat-j <catalinajuarros@gmail.com>
2018-08-13 12:12:43 -0300

    Remove debug messages

--- src/main/java/org/dogethereum/agents/core/SuperblockDefenderClient.java
+++ src/main/java/org/dogethereum/agents/core/SuperblockDefenderClient.java
@@ -75,7 +75,6 @@ public class SuperblockDefenderClient extends SuperblockBaseClient {
      * @throws Exception
      */
     private void confirmEarliestApprovableSuperblock() throws Exception {
-        log.debug("///// Confirm earliest approvable superblock");
         Keccak256Hash bestSuperblockId = ethWrapper.getBestSuperblockId();
         Superblock chainHead = superblockChain.getChainHead();
 
@@ -91,8 +90,6 @@ public class SuperblockDefenderClient extends SuperblockBaseClient {
             log.info("Best superblock from contracts, {}, not found in local database. Stopping.", bestSuperblockId);
         } else {
             Keccak256Hash toConfirmId = toConfirm.getSuperblockId();
-            log.debug("Superblock to confirm: {}", toConfirmId);
-            log.debug("Semi-approved: {}", ethWrapper.isSuperblockSemiApproved(toConfirmId));
 
             if (newAndTimeoutPassed(toConfirm) || inBattleAndSemiApprovable(toConfirm)) {
                 log.info("Confirming superblock {}", toConfirmId);
@@ -134,7 +131,6 @@ public class SuperblockDefenderClient extends SuperblockBaseClient {
                         queryBlockHeader.dogeBlockHash, queryBlockHeader.sessionId);
                 List<Sha256Hash> allDogeBlockHashes =
                         superblockChain.getSuperblock(queryBlockHeader.superblockId).getDogeBlockHashes();
-//                log.debug("Superblock hashes: {}", allDogeBlockHashes);
 
                 StoredBlock dogeBlock = dogecoinWrapper.getBlock(queryBlockHeader.dogeBlockHash);
                 ethWrapper.respondBlockHeader(queryBlockHeader.superblockId, queryBlockHeader.sessionId,
@@ -274,8 +270,6 @@ public class SuperblockDefenderClient extends SuperblockBaseClient {
             return false;
         } else {
             Keccak256Hash descendantId = descendant.getSuperblockId();
-            log.debug("Descendant {} found for superblock {}", descendantId, superblock.getSuperblockId());
-            log.debug("Status of descendant: {}", ethWrapper.getSuperblockStatus(descendantId));
             return (ethWrapper.isSuperblockSemiApproved(descendantId) &&
                     ethWrapper.isSuperblockSemiApproved(superblockId));
         }
---
+++ src/main/java/org/dogethereum/agents/core/SuperblockTooEarlyDefenderClient.java
@@ -0,0 +1,13 @@
+package org.dogethereum.agents.core;
+
+import lombok.extern.slf4j.Slf4j;
+import org.dogethereum.agents.core.dogecoin.Keccak256Hash;
+import org.dogethereum.agents.core.dogecoin.Superblock;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j(topic = "SuperblockTooEarlyDefenderClient")
+public class SuperblockTooEarlyDefenderClient extends SuperblockDefenderClient {
+
+    /* ---- CONFIRMING/DEFENDING ---- */
+}
