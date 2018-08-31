76cb3db8cccb0a6af4f2f442f0ad7b473513022f
Oscar Guindzberg <oscar.guindzberg@gmail.com>
2018-08-31 16:54:01 -0300

    New genesis superblock for doge mainnet

--- src/main/java/org/dogethereum/agents/constants/EthGanacheDogeMainAgentConstants.java
+++ src/main/java/org/dogethereum/agents/constants/EthGanacheDogeMainAgentConstants.java
@@ -32,12 +32,12 @@ public class EthGanacheDogeMainAgentConstants extends AgentConstants {
         minimumLockTxValue = Coin.valueOf(300000000); // 3 doge
 
         // Genesis Superblock for doge mainnet
-        Sha256Hash blocksMerkleRoot = Sha256Hash.wrap("045162592c1002fa0f6cf39085881da54b86dea2634e6b5f55d8258ad2b7ee0c");
-        BigInteger chainWork = new BigInteger("4018376769700331340387");
-        long lastDogeBlockTime = 1534537759l;
-        long previousToLastDogeBlockTime = 1534537657l;
-        Sha256Hash lastDogeBlockHash = Sha256Hash.wrap("2f3053d4292e163931b61b39b6063494ad1ec0b5820b03ef787dbec30126ab2d");
-        long lastDogeBlockBits = 436464932;
+        Sha256Hash blocksMerkleRoot = Sha256Hash.wrap("629417921bc4ab79db4a4a02b4d7946a4d0dbc6a3c5bca898dd12eacaeb8b353");
+        BigInteger chainWork = new BigInteger("4266257060811936889868");
+        long lastDogeBlockTime = 1535743139l;
+        long previousToLastDogeBlockTime = 1535743100l;
+        Sha256Hash lastDogeBlockHash = Sha256Hash.wrap("e2a056368784e63b9b5f9c17b613718ef7388a799e8535ab59be397019eff798");
+        long lastDogeBlockBits = 436759445;
         Keccak256Hash genesisSuperblockParentId = Keccak256Hash.wrap(new byte[32]); // initialised with 0s
         long superblockHeight = 0;
         BigInteger status = SuperblockUtils.STATUS_APPROVED;
