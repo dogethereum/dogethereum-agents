bf19d304f61de0cc16ea24baa9b252a3f7e46a56
sidhujag <sidhujag@gmail.com>
2019-05-22 11:37:11 -0700

    move info down

--- src/main/java/org/sysethereum/agents/core/SuperblockDefenderClient.java
+++ src/main/java/org/sysethereum/agents/core/SuperblockDefenderClient.java
@@ -154,12 +154,12 @@ public class SuperblockDefenderClient extends SuperblockBaseClient {
 
         for (EthWrapper.QueryMerkleRootHashesEvent queryMerkleRootHashes : queryMerkleRootHashesEvents) {
             if (isMine(queryMerkleRootHashes)) {
-                log.info("Merkle root hashes requested for session {}, superblock {}. Responding now.",
-                        queryMerkleRootHashes.sessionId, queryMerkleRootHashes.superblockId);
-
                 Superblock superblock = superblockChain.getSuperblock(queryMerkleRootHashes.superblockId);
                 if(superblock == null)
                     continue;
+                log.info("Merkle root hashes requested for session {}, superblock {}. Responding now.",
+                        queryMerkleRootHashes.sessionId, queryMerkleRootHashes.superblockId);
+
                 ethWrapper.respondMerkleRootHashes(queryMerkleRootHashes.superblockId, queryMerkleRootHashes.sessionId,
                         superblock.getSyscoinBlockHashes(), myAddress);
             }
