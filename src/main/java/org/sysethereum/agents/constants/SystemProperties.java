578ea9a03e51b2ef4f17c5c560951850deee2688
kbriverun <no-reply@your-mama.com>
2019-09-20 23:28:29 +0200

    SystemProperties: Remove unused methods. Notably remove `CONFIG` static field.

--- src/main/java/org/sysethereum/agents/constants/SystemProperties.java
+++ src/main/java/org/sysethereum/agents/constants/SystemProperties.java
@@ -9,8 +9,6 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Properties;
 
 /**
@@ -34,13 +32,10 @@ public class SystemProperties {
     private static final String YES = "yes";
     private static final String NO = "no";
 
-    public static final SystemProperties CONFIG = new SystemProperties();
+    private final Config config;
 
-    protected Config config;
-
-
-    private String projectVersion = null;
-    private String projectVersionModifier = null;
+    private final String projectVersion;
+    private final String projectVersionModifier;
     private AgentConstants agentConstants;
 
     public SystemProperties() {
@@ -50,7 +45,7 @@ public class SystemProperties {
             Config cmdLineConfigFile = file != null ? ConfigFactory.parseFile(new File(file)) : ConfigFactory.empty();
             logger.info("Config ( {} ): user properties from -Dsysethereum.agents.conf.file file '{}'",
                     cmdLineConfigFile.entrySet().isEmpty() ? NO : YES, file);
-            config = javaSystemProperties
+            this.config = javaSystemProperties
                     .withFallback(cmdLineConfigFile);
 
             logger.debug("Config trace: " + config.root().render(ConfigRenderOptions.defaults().
@@ -59,79 +54,13 @@ public class SystemProperties {
             Properties props = new Properties();
             InputStream is = getClass().getResourceAsStream("/version.properties");
             props.load(is);
-            this.projectVersion = props.getProperty("versionNumber");
-            this.projectVersion = this.projectVersion.replaceAll("'", "");
 
-            if (this.projectVersion == null) {
-                this.projectVersion = "-.-.-";
-            }
-
-            this.projectVersionModifier = props.getProperty("modifier");
-            this.projectVersionModifier = this.projectVersionModifier.replaceAll("\"", "");
-
+            this.projectVersion = props.getProperty("versionNumber").replaceAll("'", "");
+            this.projectVersionModifier = props.getProperty("modifier").replaceAll("\"", "");
         } catch (Exception e) {
             logger.error("Can't read config.", e);
             throw new RuntimeException(e);
         }
-    }
-
-    public Config getConfig() {
-        return config;
-    }
-
-    /**
-     * Puts a new config atop of existing stack making the options
-     * in the supplied config overriding existing options
-     * Once put this config can't be removed
-     *
-     * @param overrideOptions - atop config
-     */
-    public void overrideParams(Config overrideOptions) {
-        config = overrideOptions.withFallback(config);
-    }
-
-    /**
-     * Puts a new config atop of existing stack making the options
-     * in the supplied config overriding existing options
-     * Once put this config can't be removed
-     *
-     * @param keyValuePairs [name] [value] [name] [value] ...
-     */
-    public void overrideParams(String ... keyValuePairs) {
-        if (keyValuePairs.length % 2 != 0) {
-            throw new RuntimeException("Odd argument number");
-        }
-
-        Map<String, String> map = new HashMap<>();
-        for (int i = 0; i < keyValuePairs.length; i += 2) {
-            map.put(keyValuePairs[i], keyValuePairs[i + 1]);
-        }
-        overrideParams(map);
-    }
-
-    /**
-     * Puts a new config atop of existing stack making the options
-     * in the supplied config overriding existing options
-     * Once put this config can't be removed
-     *
-     * @param cliOptions -  command line options to take presidency
-     */
-    public void overrideParams(Map<String, String> cliOptions) {
-        Config cliConf = ConfigFactory.parseMap(cliOptions);
-        overrideParams(cliConf);
-    }
-
-    public <T> T getProperty(String propName, T defaultValue) {
-        if (!config.hasPath(propName)) {
-            return defaultValue;
-        }
-
-        String string = config.getString(propName);
-        if (string.trim().isEmpty()) {
-            return defaultValue;
-        }
-
-        return (T) config.getAnyRef(propName);
     }
 
     public AgentConstants getAgentConstants() {
@@ -181,6 +110,7 @@ public class SystemProperties {
     public String generalPurposeAndSendSuperblocksAddress() {
         return getStringProperty("general.purpose.and.send.superblocks.address", null);
     }
+
     public String generalPurposeAndSendSuperblocksUnlockPW(){
         return getStringProperty("general.purpose.and.send.superblocks.unlockpw", null);
     }
@@ -188,30 +118,39 @@ public class SystemProperties {
     public String syscoinSuperblockChallengerAddress() {
         return getStringProperty("syscoin.superblock.challenger.address", null);
     }
+
     public String syscoinSuperblockChallengerUnlockPW(){
         return getStringProperty("syscoin.superblock.challenger.unlockpw", null);
     }
+
     public String dataDirectory() {
         return getStringProperty("data.directory", null);
     }
+
     public String secondaryURL() {
         return getStringProperty("secondary.url", "https://mainnet.infura.io/v3/d178aecf49154b12be98e68e998cfb8d");
     }
+
     public String syscoinRPCUser() {
         return getStringProperty("syscoinrpc.user", "u");
     }
+
     public String syscoinRPCPassword() {
         return getStringProperty("syscoinrpc.password", "p");
     }
+
     public String syscoinRPCURL() {
         return getStringProperty("syscoinrpc.url_and_port", "http://localhost:8370/");
     }
+
     public long gasPriceMinimum() {
         return getLongProperty("gas.price.min", 0);
     }
+
     public long gasPriceMaximum() {
         return getLongProperty("gas.price.max",  0);
     }
+
     public long gasLimit() {
         return getLongProperty("gas.limit", 0);
     }
@@ -227,9 +166,11 @@ public class SystemProperties {
     protected String getStringProperty(String propertyName, String defaultValue) {
         return config.hasPath(propertyName) ? config.getString(propertyName) : defaultValue;
     }
+
     protected long getLongProperty(String propertyName, long defaultValue) {
         return config.hasPath(propertyName) ? config.getLong(propertyName) : defaultValue;
     }
+
     protected boolean getBooleanProperty(String propertyName, boolean defaultValue) {
         return config.hasPath(propertyName) ? config.getBoolean(propertyName) : defaultValue;
     }
