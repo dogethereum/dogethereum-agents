4741914fae59f211a9d83dbe0d54f172d649f8a1
kbriverun <no-reply@your-mama.com>
2019-09-20 23:23:26 +0200

    MainConfiguration: Add SystemProperties as a bean

--- src/main/java/org/sysethereum/agents/MainConfiguration.java
+++ src/main/java/org/sysethereum/agents/MainConfiguration.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.sysethereum.agents.constants.SystemProperties;
 
 @Configuration
 @Slf4j(topic = "MainConfiguration")
@@ -12,5 +13,10 @@ public class MainConfiguration {
     @Bean
     public Gson gson() {
         return new Gson();
+    }
+
+    @Bean
+    public SystemProperties systemProperties() {
+        return new SystemProperties();
     }
 }
